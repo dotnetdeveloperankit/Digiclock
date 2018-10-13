2b04fa03d1c23e7fed535c26719674b6174df482
admin <admin@DESKTOP-UJ1M34G>
2018-10-13 17:20:45 +0530

    Ankit Ki DigiClock

---
+++ Ankit/DigitalClock/DigitalClock/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace DigitalClock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            timelbl.Content = DateTime.Now.ToLongTimeString();
+            datelbl.Content = DateTime.Now.ToLongDateString();
+        }
+
+        private void Quitbtn_click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(1);
+
+        }
+    }
+}
